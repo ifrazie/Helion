e6763e59dbb35977ec5d3e0609804b00c1270819
frazi <frazi@DESKTOP-E7HQBRN>
2022-07-03 03:16:41 -0700

    Add project files.

---
+++ Kernel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Sys = Cosmos.System;
+
+namespace Helion
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            Sys.FileSystem.CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.Write("Text typed: ");
+            Console.WriteLine(input);
+        }
+    }
+}
